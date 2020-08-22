5a5d2cb19d54e8958a14b1e7e27986f22413a322
abquadir <abquadir73@yah00.com>
2020-08-22 22:08:36 +0530

    commited

---
+++ index.js
@@ -0,0 +1,13 @@
+// const menu = document.getElementById("menu")
+// const nav = document.querySelector("ul")
+// // const hamburger = document.querySelector(".bar")
+
+// menu.addEventListener("click",()=>{
+//    menu.classList.toggle("show")
+// })
+
+function myfunction(){
+   document.getElementById("menu").classList.toggle("show")
+   document.querySelector("nav").classList.toggle("show")
+
+}
